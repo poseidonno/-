e07bc23bc1d6bd05d445918172480a164de565f7
poseidonno zhu <961057241@qq.com>
2024-06-06 18:25:09 +0800

    更新了WiFi控制风扇转动的逻辑

--- MCU/main.py
+++ MCU/main.py
@@ -14,8 +14,42 @@ isStart = 0
 threshold_low = 15
 threshold_high = 30
 dire_fan = 1
+tempAndRh = [0, 0]
+value_TempAndRh = 1  # 初始湿度
+# 矩阵引脚
+row1 = Pin(19, Pin.OUT)
+row2 = Pin(18, Pin.OUT)
+row3 = Pin(5, Pin.OUT)
+row4 = Pin(17, Pin.OUT)
+row_list = [row1, row2, row3, row4]
+
+col1 = Pin(16, Pin.IN, Pin.PULL_DOWN)
+col2 = Pin(4, Pin.IN, Pin.PULL_DOWN)
+col3 = Pin(2, Pin.IN, Pin.PULL_DOWN)
+col4 = Pin(15, Pin.IN, Pin.PULL_DOWN)
+col_list = [col1, col2, col3, col4]
+
+names = [
+    ["1", "2", "3", "A"],
+    ["4", "5", "6", "B"],
+    ["7", "8", "9", "C"],
+    ["*", "0", "#", "D"]
+]
+
+last_state = [[0 for j in range(4)] for i in range(4)]
+
 
 # 定义GPIO引脚
+# 控制1个电机
+def auto(value):  # 温度或者湿度
+    if (value <= threshold_low):
+        motor_start(500)
+    elif (value > threshold_low and value <= threshold_high):
+        motor_start(800)
+    elif (value > threshold_high):
+        motor_start(1023)
+    print("最高阈值" + str(threshold_high) + "最低阈值" + str(threshold_low))
+
 
 def motor_shutdown():
     # 2个电机停止转动
@@ -29,12 +63,6 @@ def motor_start(num):
 
     p25.value(1 - dire_fan)
     p26.value(dire_fan)
-
-
-while isStart != 2:
-    motor_shutdown()
-    if isStart == 0:
-        isStart = 1
 
 
 ap = network.WLAN(network.MODE_11B)
@@ -68,16 +96,66 @@ except:
     ready = select.select([sock], [], [], 1)
 
 last_state = [[0 for j in range(4)] for i in range(4)]
+
+while True:
     # 接收客户端消息
     if ready[0]:
         # 有数据可读，接收数据
         data, addr = sock.recvfrom(1024)
         print(f'收到来自客户端 {addr} 的消息：{data.decode()}')
+        action = data.decode().split("-")[0]
+        if action == "getmsg":
+            sock.sendto(
+                (str(tempAndRh[0]) + "-" + str(tempAndRh[1]) + "-" + str(threshold_low) + "-" + str(threshold_high)).encode(),
+                addr)
+        if (action == "开"):
+            sock.sendto(
+                (str(tempAndRh[0]) + "-" + str(tempAndRh[1]) + "-" + str(threshold_low) + "-" + str(threshold_high)).encode(),
+                addr)
+            isAuto = False
+            power = Pin(2, Pin.OUT)
+            power.on()
+            motor_start(int(data.decode().split("-")[1]))
+        if (action == "关"):
+            sock.sendto(
+                (str(tempAndRh[0]) + "-" + str(tempAndRh[1]) + "-" + str(threshold_low) + "-" + str(threshold_high)).encode(),
+                addr)
+            isAuto = False
+            power = Pin(2, Pin.OUT)
+            power.off()
+            motor_shutdown()
+        if (action == "温湿切换"):
+            value_TempAndRh = int(data.decode().split("-")[1])
+            sock.sendto(
+                (str(tempAndRh[0]) + "-" + str(tempAndRh[1]) + "-" + str(threshold_low) + "-" + str(threshold_high)).encode(),
+                addr)
+
+        if (action == "反转"):
+            isAuto = False
+            sock.sendto(
+                (str(tempAndRh[0]) + "-" + str(tempAndRh[1]) + "-" + str(threshold_low) + "-" + str(threshold_high)).encode(),
+                addr)
+            dire_fan = 1 - dire_fan
+            motor_start(800)
+        if (action == "setvalue"):
+            isAuto = True
+            sock.sendto(
+                (str(tempAndRh[0]) + "-" + str(tempAndRh[1]) + "-" + str(threshold_low) + "-" + str(threshold_high)).encode(),
+                addr)
+            if (data.decode().split("-")[1] == "1"):
+                threshold_low = int(data.decode().split("-")[2])
+            else:
+                threshold_high = int(data.decode().split("-")[2])
+
+        ready = select.select([sock], [], [], 1)
     else:
         # 没有数据可读，发生了超时
         print('接收数据超时')
         ready = select.select([sock], [], [], 1)
- 
+
+
+
+
 
 
 
